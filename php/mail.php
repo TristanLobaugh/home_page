1ded399008caacbbb9acb48f5a6584b6ce1df9a2
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-28 17:23:47 -0400

    fixed email

--- php/mail.php
+++ php/mail.php
@@ -4,10 +4,10 @@
       $mail = new PHPMailer(); // defaults to using php "mail()"
       $body = $_POST['message'];
       $mail->AddReplyTo($_POST['email'],$_POST['name']);
-      $mail->SetFrom($_POST['email'],$_POST['name']);
+      $mail->SetFrom('hello@tristanlobaugh.com');
       $mail->AddAddress("tlobaugh@gmail.com", "Tristan Lobaugh");
       $mail->Subject    = "Message from TristanLobaugh.com";
-      $mail->MsgHTML("Message: " . $body);
+      $mail->MsgHTML("Message From: " . $_POST['email'] . ' ' . $body);
 
       if(!$mail->Send()) {
       echo "Mailer Error: " . $mail->ErrorInfo;
