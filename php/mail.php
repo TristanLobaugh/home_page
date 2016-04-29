a00bf4772fb983b8a28d7da4c40bf54af66ffe0a
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-29 11:27:30 -0400

    updated php to include name in email

--- php/mail.php
+++ php/mail.php
@@ -7,7 +7,7 @@
       $mail->SetFrom('hello@tristanlobaugh.com');
       $mail->AddAddress("tlobaugh@gmail.com", "Tristan Lobaugh");
       $mail->Subject    = "Message from TristanLobaugh.com";
-      $mail->MsgHTML("Message From: " . $_POST['email'] . ' ' . $body);
+      $mail->MsgHTML("Message From: " . $_POST['name'] . "  | Email: " . $_POST['email'] . " | "  . $body);
 
       if(!$mail->Send()) {
       echo "Mailer Error: " . $mail->ErrorInfo;
