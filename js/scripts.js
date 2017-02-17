fbc6204e6b710e113f718dcbb15f7cef7b01b1c0
TristanLobaugh <ttrader_atl@yahoo.com>
2017-02-16 23:20:54 -0500

    added portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,19 @@ $(document).ready(function(){
 	});
 
 
-	$.firefly({
-		color: '#ff0000',
-		minPixel: 1,
-		maxPixel: 3,
-		total : 65,
-		on: 'header'
-	});
-	if($(window).width() < 768){
-		$("#secondary-navigation li").css("width", "40%");
-	}
-	if($(window).width() > 992 ){
-		$("#page-wrapper").css("margin-left", "16.666666%");
-	}
+	// $.firefly({
+	// 	color: '#ff0000',
+	// 	minPixel: 1,
+	// 	maxPixel: 3,
+	// 	total : 65,
+	// 	on: 'header'
+	// });
+	// if($(window).width() < 768){
+	// 	$("#secondary-navigation li").css("width", "40%");
+	// }
+	// if($(window).width() > 992 ){
+	// 	$("#page-wrapper").css("margin-left", "16.666666%");
+	// }
 
 	$("#contact-form-btn").click(function(){
 		$("#contact-form").submit();
