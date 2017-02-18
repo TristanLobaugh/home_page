6ea1c2b6598f444c4a09ac32a238ee4df6ca32f8
TristanLobaugh <ttrader_atl@yahoo.com>
2017-02-18 02:46:53 -0500

    switched weather app and added phone to mailer

--- php/mail.php
+++ php/mail.php
@@ -11,7 +11,7 @@
       $mail->SetFrom('hello@tristanlobaugh.com');
       $mail->AddAddress("ttrader_atl@yahoo.com", "Tristan Lobaugh");
       $mail->Subject    = "Message from TristanLobaugh.com";
-      $mail->MsgHTML("Message From: " . $_POST['name'] . "  | Email: " . $_POST['email'] . " | "  . $body);
+      $mail->MsgHTML("Message From: " . $_POST['name'] . "  | Email: " . $_POST['email'] . " | Phone: " . $_POST['phone'] . " | "  . $body);
 
       if(!$mail->Send()) {
       echo "Mailer Error: " . $mail->ErrorInfo;
