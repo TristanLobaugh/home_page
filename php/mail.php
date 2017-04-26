389db4ba4200d9ecbfc2c2c858f745a846d09edc
TristanLobaugh <ttrader_atl@yahoo.com>
2017-04-26 11:47:58 -0400

    added captcha code to php

--- php/mail.php
+++ php/mail.php
@@ -4,20 +4,29 @@
       ini_set('display_startup_errors', 1);
       error_reporting(E_ALL);
 
-      require_once('/usr/share/php/libphp-phpmailer/class.phpmailer.php');
-      $mail = new PHPMailer(); // defaults to using php "mail()"
-      $body = $_POST['message'];
-      $mail->AddReplyTo($_POST['email'],$_POST['name']);
-      $mail->SetFrom('hello@tristanlobaugh.com');
-      $mail->AddAddress("ttrader_atl@yahoo.com", "Tristan Lobaugh");
-      $mail->Subject    = "Message from TristanLobaugh.com";
-      $mail->MsgHTML("Message From: " . $_POST['name'] . "  | Email: " . $_POST['email'] . " | Phone: " . $_POST['phone'] . " | "  . $body);
+      $url = 'https://www.google.com/recaptcha/api/siteverify';
+      $privatekey = "--- Key ---";
+      $response = file_get_contents($url."?secret=".$privatekey."&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']);
+      $data = json_decode($response);
 
-      if(!$mail->Send()) {
-      echo "Mailer Error: " . $mail->ErrorInfo;
-      } else {
-      echo "Message sent!";
+      if(isset($data->success) AND $data->success==true){
+            require_once('/usr/share/php/libphp-phpmailer/class.phpmailer.php');
+            $mail = new PHPMailer(); // defaults to using php "mail()"
+            $body = $_POST['message'];
+            $mail->AddReplyTo($_POST['email'],$_POST['name']);
+            $mail->SetFrom('hello@tristanlobaugh.com');
+            $mail->AddAddress("ttrader_atl@yahoo.com", "Tristan Lobaugh");
+            $mail->Subject    = "Message from TristanLobaugh.com";
+            $mail->MsgHTML("Message From: " . $_POST['name'] . "  | Email: " . $_POST['email'] . " | Phone: " . $_POST['phone'] . " | "  . $body);
+
+            if(!$mail->Send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+            } else {
+            echo "Message sent!";
+            }
+            header('location: /#contact?CaptchaPass=True');
+      }else{
+            header('location: /#contact?CaptchaFail=True');
       }
-      header('location: /#contact');
 
 ?>
