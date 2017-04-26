0e8cad97f12237f09eeabe2e2ee3f7492045eb23
TristanLobaugh <ttrader_atl@yahoo.com>
2017-04-26 10:45:03 -0400

    activate php file

--- php/mail.php
+++ php/mail.php
@@ -1,23 +1,23 @@
 <?php
       
-      // ini_set('display_errors', 1);
-      // ini_set('display_startup_errors', 1);
-      // error_reporting(E_ALL);
+      ini_set('display_errors', 1);
+      ini_set('display_startup_errors', 1);
+      error_reporting(E_ALL);
 
-      // require_once('/usr/share/php/libphp-phpmailer/class.phpmailer.php');
-      // $mail = new PHPMailer(); // defaults to using php "mail()"
-      // $body = $_POST['message'];
-      // $mail->AddReplyTo($_POST['email'],$_POST['name']);
-      // $mail->SetFrom('hello@tristanlobaugh.com');
-      // $mail->AddAddress("ttrader_atl@yahoo.com", "Tristan Lobaugh");
-      // $mail->Subject    = "Message from TristanLobaugh.com";
-      // $mail->MsgHTML("Message From: " . $_POST['name'] . "  | Email: " . $_POST['email'] . " | Phone: " . $_POST['phone'] . " | "  . $body);
+      require_once('/usr/share/php/libphp-phpmailer/class.phpmailer.php');
+      $mail = new PHPMailer(); // defaults to using php "mail()"
+      $body = $_POST['message'];
+      $mail->AddReplyTo($_POST['email'],$_POST['name']);
+      $mail->SetFrom('hello@tristanlobaugh.com');
+      $mail->AddAddress("ttrader_atl@yahoo.com", "Tristan Lobaugh");
+      $mail->Subject    = "Message from TristanLobaugh.com";
+      $mail->MsgHTML("Message From: " . $_POST['name'] . "  | Email: " . $_POST['email'] . " | Phone: " . $_POST['phone'] . " | "  . $body);
 
-      // if(!$mail->Send()) {
-      // echo "Mailer Error: " . $mail->ErrorInfo;
-      // } else {
-      // echo "Message sent!";
-      // }
-      // header('location: /#about');
+      if(!$mail->Send()) {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+      } else {
+      echo "Message sent!";
+      }
+      header('location: /#about');
 
 ?>
